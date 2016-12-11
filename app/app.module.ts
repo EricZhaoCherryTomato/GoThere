9fcae29373fc143f14f4742e0209e3b80ccfd931
Zhao Ye <eric.ye.zhao@gmail.com>
2016-12-11 12:17:16 +1000

    more test

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { UserService }      from './model';
 import { AppComponent }  from './app.component';
 
 @NgModule({
